4cc598282df13fd188ccbd63a8b0fe92a6e9d038
awojna <36032194+awojna@users.noreply.github.com>
2018-05-15 17:04:58 +0200

    Copyright info updated in added SGM source files

--- src/main/java/rseslib/simplegrid/node/Node.java
+++ src/main/java/rseslib/simplegrid/node/Node.java
@@ -1,12 +1,23 @@
 /*
- * $Name:  $
- * $RCSfile: Node.java,v $
- * $Revision: 1.1 $
- * $Date: 2007/07/07 10:20:11 $
- * Created on 2006-11-04
+ * Copyright (C) 2002 - 2017 Logic Group, Institute of Mathematics, Warsaw University
+ * 
+ *  This file is part of Rseslib.
  *
- * Copyright (c) 2006 Uniwersytet Warszawski
+ *  Rseslib is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Rseslib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
 package rseslib.simplegrid.node;
 
 

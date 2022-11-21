095411dcd924da92dcccd2dae69325992c30c64d
awojna <36032194+awojna@users.noreply.github.com>
2022-11-21 19:29:46 +0100

    Version changed to 3.2.5

--- src/main/java/rseslib/qmak/UI/QAboutDialog.java
+++ src/main/java/rseslib/qmak/UI/QAboutDialog.java
@@ -45,7 +45,7 @@ public class QAboutDialog extends JDialog implements ActionListener {
   private BorderLayout borderLayout2 = new BorderLayout();
   private FlowLayout flowLayout1 = new FlowLayout();
   private GridLayout gridLayout1 = new GridLayout();
-  private String product = "Qmak 3.2.5-SNAPSHOT";
+  private String product = "QMAK 3.2.5";
   private String copyright = "Copyright (C) 2005 - 2022";
   private TitledBorder titledBorder2;
   JPanel panelAuthors = new JPanel();
